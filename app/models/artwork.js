31cac6519450235ef95e50e0cdd7dbe6dc6cecf6
sangelici <sangelici94@gmail.com>
2019-11-30 11:08:40 -0500

    Build artwork schema in api model
    - add all attributes regarding each artwork object
    - add reference to owner of artwork

---
+++ app/models/artwork.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const artworkSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  medium: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true,
+  toObject: { virtuals: true },
+  toJson: { virtuals: true }
+})
+
+module.exports = mongoose.model('Artwork', artworkSchema)
