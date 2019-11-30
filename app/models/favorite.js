0bf6cc3f6362e986e5d4195b79603034c65762f6
sangelici <sangelici94@gmail.com>
2019-11-30 15:30:36 -0500

    Correct syntax error
    - 'mongoose.Scheme' to 'mongoose.Schema'

--- app/models/favorite.js
+++ app/models/favorite.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const favoriteSchema = new mongoose.Scheme({
+const favoriteSchema = new mongoose.Schema({
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
