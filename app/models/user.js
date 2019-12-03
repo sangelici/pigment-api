037b55515a51ea620fab26c73c919756687c5768
sangelici <sangelici94@gmail.com>
2019-12-03 10:27:53 -0500

    Make 'name' property required

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
   name: {
-    type: String
+    type: String,
+    required: true
   },
   email: {
     type: String,
