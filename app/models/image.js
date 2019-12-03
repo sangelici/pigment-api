c2a81cfb7919888945af9600aa1c022084baefee
sangelici <sangelici94@gmail.com>
2019-12-03 11:09:14 -0500

    Build schema in Image Model

---
+++ app/models/image.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const imageSchema = new mongoose.Schema({
+  imageName: {
+    type: String,
+    default: 'none',
+    required: true
+  },
+  imageData: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Image', imageSchema)
